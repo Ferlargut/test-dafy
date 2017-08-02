466cf52c7ceab8857c343fa1b678e05691099375
Ferlargut <ferlargut@gmail.com>
2017-08-02 16:21:02 -0500

    Primer maquetado}

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.use('/',express.static('public'));
+app.use("/static", express.static(path.join(__dirname, 'node_modules')));
+app.use("/static", express.static(path.join(__dirname, 'public/assets')));
 
 app.listen(3000, function () {
     console.log("hola puerto 3000");
